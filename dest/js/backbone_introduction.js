220a2b285fbf08f39e51f81d523b63e55992a76c
k2works <kakimomokuri@gmail.com>
2015-01-10 18:10:31 +0900

    モデル実装入門

--- dest/js/backbone_introduction.js
+++ dest/js/backbone_introduction.js
@@ -1,2 +1,29 @@
-/*! some copyright information here */
+/*! some copyright information here */// 連絡先を表すモデルの定義
+var Contact = Backbone.Model.extend({
+  defaults: {
+    firstName:'',
+    lastName:'',
+    email:''
+  }
+});
+// モデルの初期化
+var contact = new Contact({
+  firstName:'Alice',
+  lastName:'Henderson',
+  email:'alice@example.com'
+});
 
+console.log(JSON.stringify(contact,null,2));
+
+// 初期値を指定しないモデルの初期化
+var emptyContact = new Contact();
+console.log(JSON.stringify(emptyContact,null,2));
+
+// 初期化処理
+var Contact = Backbone.Model.extend({
+  initialize:function() {
+    console.log('Contactが初期化されました。');
+  }
+});
+
+var contact = new Contact();
